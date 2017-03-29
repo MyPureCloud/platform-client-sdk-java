94d16f72b83f2557b791ec080f16835d2c3563b3
InIn Devops <devops-jenkins@inin.com>
2017-03-29 22:06:56 +0000

    1.0.3-SNAPSHOT

--- build/src/main/java/com/mypurecloud/sdk/v2/model/Okta.java
+++ build/src/main/java/com/mypurecloud/sdk/v2/model/Okta.java
@@ -17,8 +17,8 @@ public class Okta  implements Serializable {
   private String id = null;
   private String name = null;
   private String certificate = null;
-  private String issuerURI = null;
   private String ssoTargetURI = null;
+  private String issuerURI = null;
   private Boolean disabled = null;
   private String selfUri = null;
 
@@ -66,23 +66,6 @@ public class Okta  implements Serializable {
 
   /**
    **/
-  public Okta issuerURI(String issuerURI) {
-    this.issuerURI = issuerURI;
-    return this;
-  }
-  
-  @ApiModelProperty(example = "null", value = "")
-  @JsonProperty("issuerURI")
-  public String getIssuerURI() {
-    return issuerURI;
-  }
-  public void setIssuerURI(String issuerURI) {
-    this.issuerURI = issuerURI;
-  }
-
-
-  /**
-   **/
   public Okta ssoTargetURI(String ssoTargetURI) {
     this.ssoTargetURI = ssoTargetURI;
     return this;
@@ -95,6 +78,23 @@ public class Okta  implements Serializable {
   }
   public void setSsoTargetURI(String ssoTargetURI) {
     this.ssoTargetURI = ssoTargetURI;
+  }
+
+
+  /**
+   **/
+  public Okta issuerURI(String issuerURI) {
+    this.issuerURI = issuerURI;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("issuerURI")
+  public String getIssuerURI() {
+    return issuerURI;
+  }
+  public void setIssuerURI(String issuerURI) {
+    this.issuerURI = issuerURI;
   }
 
 
@@ -134,15 +134,15 @@ public class Okta  implements Serializable {
     return Objects.equals(this.id, okta.id) &&
         Objects.equals(this.name, okta.name) &&
         Objects.equals(this.certificate, okta.certificate) &&
-        Objects.equals(this.issuerURI, okta.issuerURI) &&
         Objects.equals(this.ssoTargetURI, okta.ssoTargetURI) &&
+        Objects.equals(this.issuerURI, okta.issuerURI) &&
         Objects.equals(this.disabled, okta.disabled) &&
         Objects.equals(this.selfUri, okta.selfUri);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name, certificate, issuerURI, ssoTargetURI, disabled, selfUri);
+    return Objects.hash(id, name, certificate, ssoTargetURI, issuerURI, disabled, selfUri);
   }
 
   @Override
@@ -153,8 +153,8 @@ public class Okta  implements Serializable {
     sb.append("    id: ").append(toIndentedString(id)).append("\n");
     sb.append("    name: ").append(toIndentedString(name)).append("\n");
     sb.append("    certificate: ").append(toIndentedString(certificate)).append("\n");
-    sb.append("    issuerURI: ").append(toIndentedString(issuerURI)).append("\n");
     sb.append("    ssoTargetURI: ").append(toIndentedString(ssoTargetURI)).append("\n");
+    sb.append("    issuerURI: ").append(toIndentedString(issuerURI)).append("\n");
     sb.append("    disabled: ").append(toIndentedString(disabled)).append("\n");
     sb.append("    selfUri: ").append(toIndentedString(selfUri)).append("\n");
     sb.append("}");
