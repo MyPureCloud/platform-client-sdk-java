dc4f79043b548fada80695319d2e365c7c8909fa
InIn Devops <devops-jenkins@inin.com>
2017-03-30 14:37:07 +0000

    1.5.0

--- build/src/main/java/com/mypurecloud/sdk/v2/model/GreetingListing.java
+++ build/src/main/java/com/mypurecloud/sdk/v2/model/GreetingListing.java
@@ -22,11 +22,11 @@ public class GreetingListing  implements Serializable, PagedResource<Greeting> {
   private Integer pageSize = null;
   private Integer pageNumber = null;
   private Long total = null;
-  private String firstUri = null;
   private String selfUri = null;
-  private String previousUri = null;
+  private String firstUri = null;
   private String nextUri = null;
   private String lastUri = null;
+  private String previousUri = null;
   private Integer pageCount = null;
 
   
@@ -100,23 +100,6 @@ public class GreetingListing  implements Serializable, PagedResource<Greeting> {
 
   /**
    **/
-  public GreetingListing firstUri(String firstUri) {
-    this.firstUri = firstUri;
-    return this;
-  }
-  
-  @ApiModelProperty(example = "null", value = "")
-  @JsonProperty("firstUri")
-  public String getFirstUri() {
-    return firstUri;
-  }
-  public void setFirstUri(String firstUri) {
-    this.firstUri = firstUri;
-  }
-
-
-  /**
-   **/
   public GreetingListing selfUri(String selfUri) {
     this.selfUri = selfUri;
     return this;
@@ -134,18 +117,18 @@ public class GreetingListing  implements Serializable, PagedResource<Greeting> {
 
   /**
    **/
-  public GreetingListing previousUri(String previousUri) {
-    this.previousUri = previousUri;
+  public GreetingListing firstUri(String firstUri) {
+    this.firstUri = firstUri;
     return this;
   }
   
   @ApiModelProperty(example = "null", value = "")
-  @JsonProperty("previousUri")
-  public String getPreviousUri() {
-    return previousUri;
+  @JsonProperty("firstUri")
+  public String getFirstUri() {
+    return firstUri;
   }
-  public void setPreviousUri(String previousUri) {
-    this.previousUri = previousUri;
+  public void setFirstUri(String firstUri) {
+    this.firstUri = firstUri;
   }
 
 
@@ -185,6 +168,23 @@ public class GreetingListing  implements Serializable, PagedResource<Greeting> {
 
   /**
    **/
+  public GreetingListing previousUri(String previousUri) {
+    this.previousUri = previousUri;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("previousUri")
+  public String getPreviousUri() {
+    return previousUri;
+  }
+  public void setPreviousUri(String previousUri) {
+    this.previousUri = previousUri;
+  }
+
+
+  /**
+   **/
   public GreetingListing pageCount(Integer pageCount) {
     this.pageCount = pageCount;
     return this;
@@ -213,17 +213,17 @@ public class GreetingListing  implements Serializable, PagedResource<Greeting> {
         Objects.equals(this.pageSize, greetingListing.pageSize) &&
         Objects.equals(this.pageNumber, greetingListing.pageNumber) &&
         Objects.equals(this.total, greetingListing.total) &&
-        Objects.equals(this.firstUri, greetingListing.firstUri) &&
         Objects.equals(this.selfUri, greetingListing.selfUri) &&
-        Objects.equals(this.previousUri, greetingListing.previousUri) &&
+        Objects.equals(this.firstUri, greetingListing.firstUri) &&
         Objects.equals(this.nextUri, greetingListing.nextUri) &&
         Objects.equals(this.lastUri, greetingListing.lastUri) &&
+        Objects.equals(this.previousUri, greetingListing.previousUri) &&
         Objects.equals(this.pageCount, greetingListing.pageCount);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(entities, pageSize, pageNumber, total, firstUri, selfUri, previousUri, nextUri, lastUri, pageCount);
+    return Objects.hash(entities, pageSize, pageNumber, total, selfUri, firstUri, nextUri, lastUri, previousUri, pageCount);
   }
 
   @Override
@@ -235,11 +235,11 @@ public class GreetingListing  implements Serializable, PagedResource<Greeting> {
     sb.append("    pageSize: ").append(toIndentedString(pageSize)).append("\n");
     sb.append("    pageNumber: ").append(toIndentedString(pageNumber)).append("\n");
     sb.append("    total: ").append(toIndentedString(total)).append("\n");
-    sb.append("    firstUri: ").append(toIndentedString(firstUri)).append("\n");
     sb.append("    selfUri: ").append(toIndentedString(selfUri)).append("\n");
-    sb.append("    previousUri: ").append(toIndentedString(previousUri)).append("\n");
+    sb.append("    firstUri: ").append(toIndentedString(firstUri)).append("\n");
     sb.append("    nextUri: ").append(toIndentedString(nextUri)).append("\n");
     sb.append("    lastUri: ").append(toIndentedString(lastUri)).append("\n");
+    sb.append("    previousUri: ").append(toIndentedString(previousUri)).append("\n");
     sb.append("    pageCount: ").append(toIndentedString(pageCount)).append("\n");
     sb.append("}");
     return sb.toString();
--- build/src/main/java/com/mypurecloud/sdk/v2/model/SchemaReferenceEntityListing.java
+++ build/src/main/java/com/mypurecloud/sdk/v2/model/SchemaReferenceEntityListing.java
@@ -22,11 +22,11 @@ public class SchemaReferenceEntityListing  implements Serializable, PagedResourc
   private Integer pageSize = null;
   private Integer pageNumber = null;
   private Long total = null;
-  private String firstUri = null;
   private String selfUri = null;
-  private String previousUri = null;
+  private String firstUri = null;
   private String nextUri = null;
   private String lastUri = null;
+  private String previousUri = null;
   private Integer pageCount = null;
 
   
@@ -100,23 +100,6 @@ public class SchemaReferenceEntityListing  implements Serializable, PagedResourc
 
   /**
    **/
-  public SchemaReferenceEntityListing firstUri(String firstUri) {
-    this.firstUri = firstUri;
-    return this;
-  }
-  
-  @ApiModelProperty(example = "null", value = "")
-  @JsonProperty("firstUri")
-  public String getFirstUri() {
-    return firstUri;
-  }
-  public void setFirstUri(String firstUri) {
-    this.firstUri = firstUri;
-  }
-
-
-  /**
-   **/
   public SchemaReferenceEntityListing selfUri(String selfUri) {
     this.selfUri = selfUri;
     return this;
@@ -134,18 +117,18 @@ public class SchemaReferenceEntityListing  implements Serializable, PagedResourc
 
   /**
    **/
-  public SchemaReferenceEntityListing previousUri(String previousUri) {
-    this.previousUri = previousUri;
+  public SchemaReferenceEntityListing firstUri(String firstUri) {
+    this.firstUri = firstUri;
     return this;
   }
   
   @ApiModelProperty(example = "null", value = "")
-  @JsonProperty("previousUri")
-  public String getPreviousUri() {
-    return previousUri;
+  @JsonProperty("firstUri")
+  public String getFirstUri() {
+    return firstUri;
   }
-  public void setPreviousUri(String previousUri) {
-    this.previousUri = previousUri;
+  public void setFirstUri(String firstUri) {
+    this.firstUri = firstUri;
   }
 
 
@@ -185,6 +168,23 @@ public class SchemaReferenceEntityListing  implements Serializable, PagedResourc
 
   /**
    **/
+  public SchemaReferenceEntityListing previousUri(String previousUri) {
+    this.previousUri = previousUri;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("previousUri")
+  public String getPreviousUri() {
+    return previousUri;
+  }
+  public void setPreviousUri(String previousUri) {
+    this.previousUri = previousUri;
+  }
+
+
+  /**
+   **/
   public SchemaReferenceEntityListing pageCount(Integer pageCount) {
     this.pageCount = pageCount;
     return this;
@@ -213,17 +213,17 @@ public class SchemaReferenceEntityListing  implements Serializable, PagedResourc
         Objects.equals(this.pageSize, schemaReferenceEntityListing.pageSize) &&
         Objects.equals(this.pageNumber, schemaReferenceEntityListing.pageNumber) &&
         Objects.equals(this.total, schemaReferenceEntityListing.total) &&
-        Objects.equals(this.firstUri, schemaReferenceEntityListing.firstUri) &&
         Objects.equals(this.selfUri, schemaReferenceEntityListing.selfUri) &&
-        Objects.equals(this.previousUri, schemaReferenceEntityListing.previousUri) &&
+        Objects.equals(this.firstUri, schemaReferenceEntityListing.firstUri) &&
         Objects.equals(this.nextUri, schemaReferenceEntityListing.nextUri) &&
         Objects.equals(this.lastUri, schemaReferenceEntityListing.lastUri) &&
+        Objects.equals(this.previousUri, schemaReferenceEntityListing.previousUri) &&
         Objects.equals(this.pageCount, schemaReferenceEntityListing.pageCount);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(entities, pageSize, pageNumber, total, firstUri, selfUri, previousUri, nextUri, lastUri, pageCount);
+    return Objects.hash(entities, pageSize, pageNumber, total, selfUri, firstUri, nextUri, lastUri, previousUri, pageCount);
   }
 
   @Override
@@ -235,11 +235,11 @@ public class SchemaReferenceEntityListing  implements Serializable, PagedResourc
     sb.append("    pageSize: ").append(toIndentedString(pageSize)).append("\n");
     sb.append("    pageNumber: ").append(toIndentedString(pageNumber)).append("\n");
     sb.append("    total: ").append(toIndentedString(total)).append("\n");
-    sb.append("    firstUri: ").append(toIndentedString(firstUri)).append("\n");
     sb.append("    selfUri: ").append(toIndentedString(selfUri)).append("\n");
-    sb.append("    previousUri: ").append(toIndentedString(previousUri)).append("\n");
+    sb.append("    firstUri: ").append(toIndentedString(firstUri)).append("\n");
     sb.append("    nextUri: ").append(toIndentedString(nextUri)).append("\n");
     sb.append("    lastUri: ").append(toIndentedString(lastUri)).append("\n");
+    sb.append("    previousUri: ").append(toIndentedString(previousUri)).append("\n");
     sb.append("    pageCount: ").append(toIndentedString(pageCount)).append("\n");
     sb.append("}");
     return sb.toString();
