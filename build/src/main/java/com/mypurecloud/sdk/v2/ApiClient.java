e55983564f6057345c6e599c2ea2cfe1516c63b7
InIn Devops <devops-jenkins@inin.com>
2017-07-14 17:03:48 +0000

    15.1.0

--- build/src/main/java/com/mypurecloud/sdk/v2/ApiClient.java
+++ build/src/main/java/com/mypurecloud/sdk/v2/ApiClient.java
@@ -649,6 +649,7 @@ public class ApiClient implements AutoCloseable {
         private Builder(ConnectorProperties properties) {
             this.properties = (properties != null) ? properties.copy() : new ConnectorProperties();
             withUserAgent(DEFAULT_USER_AGENT);
+            withDefaultHeader("purecloud-sdk", "15.1.0");
         }
 
         public Builder withDefaultHeader(String header, String value) {
