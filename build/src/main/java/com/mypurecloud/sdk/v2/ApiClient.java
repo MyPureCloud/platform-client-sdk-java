32eb03c8c13d40b33a2510480ab867a238157208
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2021-03-04 15:55:19 +0000

    117.0.2

--- build/src/main/java/com/mypurecloud/sdk/v2/ApiClient.java
+++ build/src/main/java/com/mypurecloud/sdk/v2/ApiClient.java
@@ -871,7 +871,7 @@ public class ApiClient implements AutoCloseable {
         private Builder(ConnectorProperties properties) {
             this.properties = (properties != null) ? properties.copy() : new ConnectorProperties();
             withUserAgent(DEFAULT_USER_AGENT);
-            withDefaultHeader("purecloud-sdk", "117.0.1");
+            withDefaultHeader("purecloud-sdk", "117.0.2");
         }
 
         public Builder withDefaultHeader(String header, String value) {
